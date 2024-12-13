ef7dfa655e5b1e38bc82513723011b55d6c11b6c
Nishi <nishi@nishi.boats>
2024-12-13 21:31:18 +0000

    add
    git-svn-id: file:///raid/svn-personal/newsdist/trunk@7 6619d122-1e26-ed48-a79d-fd80d44ec7e7

--- Server/main.c
+++ Server/main.c
@@ -1,4 +1,19 @@
 /* $Id$ */
 
+#include <stdio.h>
+#include <string.h>
+
 int main(int argc, char** argv){
+	int i;
+
+	for(i = 1; i < argc; i++){
+		if(argv[i][0] == '-'){
+			if(strcmp(argv[i], "--version") == 0 || strcmp(argv[i], "-V") == 0){
+				return 0;
+			}else{
+				fprintf(stderr, "Invalid flag: %s\n", argv[i]);
+				return 1;
+			}
+		}
+	}
 }
