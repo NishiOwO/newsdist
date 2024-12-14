c89b47ce9a25cd1958de43ac9b9635e09659b2ad
Nishi <nishi@nishi.boats>
2024-12-14 14:45:06 +0000

    main
    git-svn-id: file:///raid/svn-personal/newsdist/trunk@8 6619d122-1e26-ed48-a79d-fd80d44ec7e7

--- Server/main.c
+++ Server/main.c
@@ -5,7 +5,8 @@
 
 int main(int argc, char** argv){
 	int i;
-
+	printf("NewsDist NNTP daemon - NewsDist/%s\n", NEWSDIST_VERSION);
+	printf("Under public-domain, original by Nishi <nishi@nishi.boats>.\n");
 	for(i = 1; i < argc; i++){
 		if(argv[i][0] == '-'){
 			if(strcmp(argv[i], "--version") == 0 || strcmp(argv[i], "-V") == 0){
