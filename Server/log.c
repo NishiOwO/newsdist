909fa739f4b99df4cba2d395417895051dd17425
Nishi <nishi@nishi.boats>
2024-12-16 09:44:01 +0000

    log
    git-svn-id: file:///raid/svn-personal/newsdist/trunk@19 6619d122-1e26-ed48-a79d-fd80d44ec7e7

--- Server/log.c
+++ Server/log.c
@@ -8,6 +8,7 @@
 #include "../config.h"
 
 #include <stdio.h>
+#include <time.h>
 #ifdef HAS_SYSLOG
 #include <syslog.h>
 #endif
@@ -38,7 +39,7 @@
 #ifdef LOG_FACILITY
 #ifdef HAS_LOG_INFO
 #ifdef LOG_NOTICE_LEVEL
-#define USE_SYSTEM_SYSLOG
+/* #define USE_SYSTEM_SYSLOG */
 #endif
 #endif
 #endif
@@ -50,6 +51,30 @@ nd_init_log(void)
 #ifdef USE_SYSTEM_SYSLOG
 	openlog("newsdist", LOG_PID | LOG_PERROR, LOG_FACILITY);
 #endif
+	nd_log_info("NewsDist/" NEWSDIST_VERSION " starting up");
+}
+
+void
+nd_log_string(char *out)
+{
+	time_t		t = time(NULL);
+	struct tm      *tm = localtime(&t);
+	const char     *mons[] = {
+		"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"May",
+		"Jun",
+		"Jul",
+		"Aug",
+		"Sep",
+		"Oct",
+		"Nov",
+		"Dec"
+	};
+
+	sprintf(out, "%s %.d %.2d:%02d:%02d", mons[tm->tm_mon], tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
 }
 
 void
@@ -58,6 +83,10 @@ nd_log_info(const char *info)
 #ifdef USE_SYSTEM_SYSLOG
 	syslog(LOG_INFO, info);
 #else
+	char		timestr[512];
+
+	nd_log_string(timestr);
+	fprintf(stderr, "%s [ info ] %s\n", timestr, info);
 #endif
 }
 
@@ -67,5 +96,9 @@ nd_log_notice(const char *info)
 #ifdef USE_SYSTEM_SYSLOG
 	syslog(LOG_NOTICE_LEVEL, info);
 #else
+	char		timestr[512];
+
+	nd_log_string(timestr);
+	fprintf(stderr, "%s [notice] %s\n", timestr, info);
 #endif
 }
