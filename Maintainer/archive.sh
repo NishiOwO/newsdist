a9f683771ec014fa3ee86a5f928fa4d7a3eca427
Nishi <nishi@nishi.boats>
2024-12-30 08:29:16 +0000

    adding archive stuff
    git-svn-id: file:///raid/svn-personal/newsdist/trunk@147 6619d122-1e26-ed48-a79d-fd80d44ec7e7

---
+++ Maintainer/archive.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# $Id$
+CONFIG="GET"
+OLD=`pwd`
+. ./configure
+make distclean || true
+echo "Packing for version $VERSION"
+rm -rf /tmp/newsdist-$VERSION
+mkdir -p /tmp/newsdist-$VERSION
+cp -rf * /tmp/newsdist-$VERSION/
+rm -f *.tar.gz
+cd /tmp
+tar czvf $OLD/newsdist-$VERSION.tar.gz newsdist-$VERSION
